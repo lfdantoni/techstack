cf456f319650042d0c7273d44f4aa99dc088f525
lfdantoni <lfdantoni@gmail.com>
2017-09-05 00:03:34 -0300

    add reducer and LibraryList component

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
+import {LibraryList}  from './components/';
 
 const App = () => {
     return (
-        <Provider store={createStore(() => [])}>
-            <View></View>
+        <Provider store={createStore(reducers)}>
+            <View>
+                <LibraryList />
+            </View>
         </Provider>
     )
 }
